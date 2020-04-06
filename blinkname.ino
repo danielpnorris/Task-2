f5077735d96462b40448cbd53367e21c4abbe8b1
danielpnorris <62794374+danielpnorris@users.noreply.github.com>
2020-04-06 17:20:59 +1000

    Add files via upload

---
+++ blinkname.ino
@@ -0,0 +1,185 @@
+
+
+int led1 = D0; 
+
+int led2 = D7; 
+
+
+void setup() {
+
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+}
+
+
+void loop() {
+    // D - . .
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(1000);
+
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500);  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(1000);  
+ // A . -
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500);  
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(1000); 
+// N - .
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500); 
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(1000);
+// I . .
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500);  
+  
+    digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(1000);  
+// E .
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(1000);  
+// L . - . .
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500);  
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(1000);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500); 
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(500);  
+  
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+
+
+  delay(500);
+  
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+
+
+  delay(1000);  
+}
+
